dded9d9b05de1e951e15796b0ade0b4dac349bfc
Kristi <kking@attivio.com>
2019-03-20 09:14:07 -0400

    chore/PLAT-0: Bring in istanbul coverage instrumenter for more accurate code coverage assessment and preprocessing tooling

--- nwb.config.js
+++ nwb.config.js
@@ -14,12 +14,18 @@ module.exports = {
     config(config) {
       const newConfig = { ...config };
       newConfig.files = [
-        'src/**/*.js',
-        'src/**/*.jsx',
-        'tests/**/*.js',
+        { pattern: 'src/**/*.js', type: 'module' },
+        { pattern: 'src/**/*.jsx', type: 'module' },
+        { pattern: 'tests/**/*.js', type: 'module' },
+      ];
+      newConfig.frameworks = ['mocha'];
+      newConfig.plugins = [
+        require('karma-coverage-istanbul-instrumenter'),
+        require('karma-coverage-istanbul-reporter'),
+        require('karma-mocha'),
       ];
       // coverage reporter generates the coverage
-      newConfig.reporters = ['progress', 'coverage'];
+      newConfig.reporters = ['progress', 'coverage-istanbul'];
       newConfig.preprocessors = {
         // source files, that you wanna generate coverage for
         // do not include tests or libraries
@@ -28,12 +34,26 @@ module.exports = {
       };
       newConfig.coverageIstanbulInstrumenter = {
         esModules: true,
+        plugins: [
+          'asyncGenerators',
+          'classProperties',
+          'dynamicImport',
+          'exportDefaultFrom',
+          'objectRestSpread',
+          'optionalCatchBinding',
+          'flow',
+          'jsx',
+        ],
       };
 
       newConfig.coverageIstanbulReporter = {
         reports: ['html'],
         dir: path.join(__dirname, 'coverage'),
       };
+
+      newConfig.browsers = ['ChromeHeadless'];
+      newConfig.singleRun = true;
+
       return newConfig;
     },
   },
